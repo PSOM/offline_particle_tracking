e4999a5483058930ce3e516aabb2653dd788e152
Mat Dever <mathieu.dever@dal.ca>
2017-11-24 09:46:55 -0500

    updated getpartivar.m
    Major modifications to getpartivar.m
    * Velocity field onto particle position is computed using the centered
    velocity field (which is NOT divergence-free). This does not affect the
    advection of particles, centered velocities are only used for recording
    particle characteristics
    * Added potential vorticity and relative vorticity to the list of
    variables to interpolate onto particle position

--- getpartivar.m
+++ getpartivar.m
@@ -8,24 +8,45 @@ disp('INTERPOLATE VARIABLES ONTO PARTICLES... ')
 
 parti.doy(:) = tt;
 
-% Velocity field
-F = griddedInterpolant(model.ufx,model.ufy,model.ufz,model.uf);
-parti.u = F(parti.x,parti.y,parti.z);
-F = griddedInterpolant(model.vfx,model.vfy,model.vfz,model.vf);
-parti.v = F(parti.x,parti.y,parti.z);
-F = griddedInterpolant(model.wfx,model.wfy,model.wfz,model.wf);
-parti.w = F(parti.x,parti.y,parti.z);
+%% Velocity field
 
-% Hydrography
+% If velocity field extracted from face velocity-fluxes (divergence free)
+
+% F = griddedInterpolant(model.ufx,model.ufy,model.ufz,model.uf);
+% parti.u = F(parti.x,parti.y,parti.z);
+% F = griddedInterpolant(model.vfx,model.vfy,model.vfz,model.vf);
+% parti.v = F(parti.x,parti.y,parti.z);
+% F = griddedInterpolant(model.wfx,model.wfy,model.wfz,model.wf);
+% parti.w = F(parti.x,parti.y,parti.z);
+
+% If velocity field extracted from centered velocities (NOT divergence
+% free)
+F = griddedInterpolant(model.x2,model.y2,model.z2,model.doy2,model.u);
+parti.u = F(parti.x,parti.y,parti.z,parti.doy);
+F = griddedInterpolant(model.x2,model.y2,model.z2,model.doy2,model.v);
+parti.v = F(parti.x,parti.y,parti.z,parti.doy);
+F = griddedInterpolant(model.x2,model.y2,model.z2,model.doy2,model.w);
+parti.w = F(parti.x,parti.y,parti.z,parti.doy);
+
+%% Hydrography
 F = griddedInterpolant(model.x2,model.y2,model.z2,model.doy2,model.T);
 parti.t = F(parti.x,parti.y,parti.z,parti.doy);
 F = griddedInterpolant(model.x2,model.y2,model.z2,model.doy2,model.S);
 parti.s = F(parti.x,parti.y,parti.z,parti.doy);
 F = griddedInterpolant(model.x2,model.y2,model.z2,model.doy2,model.RHO);
 parti.rho = F(parti.x,parti.y,parti.z,parti.doy);
+
+%% Potential-vorticity
+F = griddedInterpolant(model.x2,model.y2,model.z2,model.doy2,model.pv);
+parti.pv = F(parti.x,parti.y,parti.z,parti.doy);
+
+%% Relative vorticity
+F = griddedInterpolant(model.x2,model.y2,model.z2,model.doy2,model.vor);
+parti.vor = F(parti.x,parti.y,parti.z,parti.doy);
 clear F
 parti.wtotal = parti.wsink+parti.w;
 
+%%
 disp('DONE')
 disp('%%%%%%%%%%%%%%%%%')
 disp(' ')
