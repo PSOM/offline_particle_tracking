da3f0b7ebc96e4e093ad41214363706644b331c4
Mat Dever <mathieu.dever@dal.ca>
2017-11-24 09:56:45 -0500

    Updated iniparticles.m
    Major modifications to iniparticles.m:
    * Simplification of the code used to assign (x,y,z) to particles when
    seeding
    * Includes potential and relative vorticities variables

--- iniparticles.m
+++ iniparticles.m
@@ -21,13 +21,15 @@ if tt == particle.initime
     parti.s = [];
     parti.t = [];
     parti.rho = [];
+    parti.pv = [];
+    parti.vor = [];
 end
 
 % Number of particle to seed
 nprtoseed = (particle.irange/particle.irez)*(particle.jrange/particle.jrez)*(particle.krange/particle.krez)*particle.numofclasses;
 disp(['Seeding ',num2str(nprtoseed),' particles'])
 
-% particle day of year
+% Particle day of year
 parti.doy = cat(1,parti.doy,tt*ones(nprtoseed,1));
 
 % Particle ID
@@ -35,30 +37,30 @@ blop = ((tt-particle.initime)/particle.inifreq*nprtoseed+1:(tt-particle.initime)
 parti.id = cat(1,parti.id,blop'); clear blop
 
 for theclass = 1:particle.numofclasses
-    % Partcle seeding location
-    parti.x = cat(1,parti.x,repmat(particle.istart:particle.irez:particle.istart + ...
+    % Particle seeding location
+    %     parti.x = cat(1,parti.x,repmat(particle.istart:particle.irez:particle.istart + ...
+    %         particle.irange-particle.irez,...
+    %         [1 particle.jrange/particle.jrez*particle.krange/particle.krez])');
+    %     blop = repmat(particle.jstart:particle.jrez:particle.jstart + ...
+    %         particle.jrange-particle.jrez,...
+    %         [particle.irange/particle.irez*particle.krange/particle.krez 1]);
+    %     blop = blop(:);
+    %     parti.y = cat(1,parti.y,blop); clear blop
+    %
+    %     parti.z = cat(1,parti.z,repmat(particle.kstart:particle.krez:particle.kstart + ...
+    %         particle.krange-particle.krez,...
+    %         [1 particle.jrange/particle.jrez*particle.irange/particle.irez])');
+    [x,y,z] = meshgrid(particle.istart:particle.irez:particle.istart + ...
         particle.irange-particle.irez,...
-        [1 particle.jrange/particle.jrez])');
-    blop = repmat(particle.jstart:particle.jrez:particle.jstart + ...
+        particle.jstart:particle.jrez:particle.jstart + ...
         particle.jrange-particle.jrez,...
-        [particle.irange/particle.irez 1]);
-    blop = blop(:);
-    parti.y = cat(1,parti.y,blop); clear blop
-    parti.z = cat(1,parti.z,repmat(particle.kstart:particle.krez:particle.kstart + ...
-        particle.krange-particle.krez,...
-        [1 particle.jrange/particle.jrez*particle.irange/particle.irez])');
+        particle.kstart:particle.krez:particle.kstart + ...
+        particle.krange-particle.krez);
     
-%     % check that particles are released in domain
-%     if isempty(find(parti.x<min(model.x) | parti.x>max(model.x)))==0
-%         error([num2str(length(find(parti.x<min(model.x) | parti.x>max(model.x)))), 'particles are seeded out of the domain'])
-%     end
-%     if isempty(find(parti.y<min(model.y) | parti.y>max(model.y)))==0
-%         error([num2str(length(find(parti.y<min(model.y) | parti.y>max(model.y)))), 'particles are seeded out of the domain'])
-%     end
-%     if isempty(find(parti.z<min(model.z) | parti.z>max(model.z)))==0
-%         error([num2str(length(find(parti.z<min(model.z) | parti.z>max(model.z)))), 'particles are seeded out of the domain'])
-%     end
-    
+    parti.x = cat(1,parti.x,x(:));
+    parti.y = cat(1,parti.y,y(:));
+    parti.z = cat(1,parti.z,z(:));
+    clear x y z
     % Prescribe sinking velocity (in m/s)
     if theclass == 1
         parti.wsink = cat(1,parti.wsink,zeros(nprtoseed/particle.numofclasses,1));
@@ -85,7 +87,9 @@ parti.wtotal = cat(1,parti.wtotal,parti.w+parti.wsink);
 parti.t = cat(1,parti.t,zeros(nprtoseed,1));
 parti.s = cat(1,parti.s,zeros(nprtoseed,1));
 parti.rho = cat(1,parti.rho,zeros(nprtoseed,1));
-clear nprtoseed 
+parti.pv = cat(1,parti.pv,zeros(nprtoseed,1));
+parti.vor = cat(1,parti.vor,zeros(nprtoseed,1));
+clear nprtoseed
 
 disp('DONE')
 disp('%%%%%%%%%%%%%%%%%')
